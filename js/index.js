b25d7631bf4142ea471bebe7d1ee84ff92a6640d
inesgarper <igperianez@gmail.com>
2022-01-28 11:22:40 +0100

    fixed bugs

--- js/index.js
+++ js/index.js
@@ -3,5 +3,6 @@ const canvas = document.querySelector('.hidden')
 
 startButton.addEventListener('click', () => {
     canvas.setAttribute('class', 'showing')
+    document.activeElement.blur()
     Game.gameInit()
 })
